62baa0afc7e4db72a646d5919335f2277cf94aea
“sawinee-srisakul” <sawinee.srisakul@gmail.com>
2025-07-29 18:27:14 +1000

    Expose Flask app globally for Render deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from sawineecosmetic import create_app
 
-if __name__=='__main__':
-    napp=create_app()
+napp = create_app()  # Create the app globally
+
+if __name__ == '__main__':
     napp.run(debug=True)
-    
